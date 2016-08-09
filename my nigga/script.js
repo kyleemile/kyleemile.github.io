a8cf16500d29470b989a261ecbb8d0d04e38e2d0
Kyle <kyle.emile@gmail.com>
2016-08-09 21:42:42 +0000

    geometric pattern

--- my nigga/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('go').onclick = function() {
-    var inputText = $('#text-input').val();
-    var thugifiedText = inputText + ", my nigga.";
-    document.getElementById('output').innerHTML = thugifiedText;
-};
